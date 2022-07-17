6b92f96d8321606b5f63eaa7ad0f9d7ad38bb3b4
SiroSanjaya22 <109472474+SiroSanjaya22@users.noreply.github.com>
2022-07-17 12:08:06 -0600

    Update Hero.js
    Done Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,49 +1,24 @@
-import { Carousel } from 'react-bootstrap';
-import '../style/hero.css';
-import foto from '../assets/spiderman.jpg';
+/* eslint-disable jsx-a11y/alt-text */
 
+import '../style/hero.css';
+import foto from '../assets/logo192.png';
 
 function Hero() {
     return (
-      <Carousel fade>
-        <Carousel.Item>
-          <img
-            className="hero"
-            src="https://cutewallpaper.org/27/black-hole-gif-wallpaper/artstation-arcane-absortion-black-hole-alvaro-g-a-black-hole-wallpaper-black-hole-gif-black-hole.gif"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="hero"
-            src={ foto }
-            alt="Second slide"
-          />
-  
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="hero"
-            src={ foto }
-            alt="Third slide"
-          />
-  
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <div className="Hero"> 
+      <center>
+        <div className="tittle">
+      <img src={foto}/>
+        <h1 className="titleWelcome">
+        Sazanaz Haver <br/></h1>
+      <p>
+      With Best Staycasion
+      </p>
+    
+    </div>  
+    </center>
+          </div>
+ 
     );
   }
 
