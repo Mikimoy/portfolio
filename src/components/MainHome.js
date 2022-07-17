96dac7bf130c0103023504995646f9e287c33a20
Holycan <48346790+Mikimoy@users.noreply.github.com>
2022-07-18 01:37:18 +0700

    tai

--- src/components/MainHome.js
+++ src/components/MainHome.js
@@ -1,17 +1,48 @@
 import React from 'react'
 import '../style/mainhome.css';
 import { Row, Col, Container } from 'react-bootstrap';
+import services from '../assets/logo.png';
 
 
 const MainHome = () => {
   return (
     <div className="main">
     <Container>
-      <Row>
-        <Col sm>Website Developers <br></br><p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p></Col>
-        <Col sm>3D Artist<br></br><p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p></Col>
-        <Col sm>Game Developers<br></br><p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p></Col>
-      </Row>
+      <Row md={4}>
+      <Col sm className='coll1'>
+      <div>
+        <img src={ services } alt='logo' className='img-web-dev'/>
+        </div>
+        <div>
+        <h1>Web Developer</h1>
+        </div>
+        <div>
+        <p>"Neque porro quisquam est qui dolorem ipsum<br/>quia dolor sit amet, consectetur, adipisci velit..."</p>
+        </div>
+        </Col>
+        <Col sm className='coll2'>
+        <div>
+        <img src={ services } alt='logo' className='img-game-dev'/>
+        </div>
+        <div>
+        <h1>Game Developer</h1>
+        </div>
+        <div>
+        <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+        </div>
+        </Col>
+        <Col sm className='coll3'>
+        <div>
+        <img src={ services } alt='logo' className='img-3d-dev'/>
+        </div>
+        <div>
+        <h1>3D Artist</h1>
+        </div>
+        <div>
+        <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+        </div>
+        </Col>
+        </Row>
     </Container>
     </div>
   )
