0557f886ec6132fdd8840defa6da688f640c249e
SiroSanjaya22 <109472474+SiroSanjaya22@users.noreply.github.com>
2022-07-18 09:29:07 -0600

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import MainHome from '../components/MainHome';
-
+import Footer from  '../components/Footer';
 const Home = () => {
   return (
     <div>
         <Header />
             <Hero />
             <MainHome />
+            <Footer/>
     </div>
   )
 }
