78403730d53a7a22b94259da75342891933c04fd
SiroSanjaya22 <109472474+SiroSanjaya22@users.noreply.github.com>
2022-07-19 07:36:47 -0600

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import MainHome from '../components/MainHome';
+import ImageOne from '../components/Parallax';
 import Footer from  '../components/Footer';
 const Home = () => {
   return (
@@ -9,6 +10,11 @@ const Home = () => {
         <Header />
             <Hero />
             <MainHome />
+            <section>
+            <ImageOne/>
+            </section>
+            <MainHome />
+            <ImageOne/>
             <Footer/>
     </div>
   )
