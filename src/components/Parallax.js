8ebe7f691090bf3731a8d8d2add5feee4102bb94
SiroSanjaya22 <109472474+SiroSanjaya22@users.noreply.github.com>
2022-07-20 10:05:08 -0600

    Update Parallax.js

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -1,11 +1,36 @@
 import { Parallax } from 'react-parallax';
 import Nasa from '../assets/nasa.jpg'
 import '../style/Parallax.css';
+
+import { Row, Col, Container } from 'react-bootstrap';
+import foto from '../assets/logo192.png';
+
 const ImageOne = () => (
     <Parallax className='image ' blur={0} bgImage={Nasa}  strength={800} bgImageStyle={{minHeight:"120vh" }}>
-        <div className='content'>
-            <span className="img-txt">a trip tasdsadsado Space</span>
-        </div>
+      <div >
+         <Container className='Card'>
+         <Row>
+        <Col sm className='ada'>
+          <h2>Our Service</h2>
+          <hr></hr>
+        </Col>
+        <Col sm className='ada'>  <i class="fa-solid fa-browser" ></i><h5>Web Developer</h5>
+        <p>A company profile describes what makes your company unique. It automatically differentiates your brand because no other company has the exact same founding story  </p>
+
+        <i class="fa-solid fa-browser" ></i><h5>Web Developer</h5>
+        <p>A company profile describes what makes your company unique. It automatically differentiates your brand because no other company has the exact same founding story  </p>
+        <i class="fa-solid fa-browser" ></i><h5>Web Developer</h5>
+        <p>A company profile describes what makes your company unique. It automatically differentiates your brand because no other company has the exact same founding story  </p>
+        </Col>
+        <Col sm className='ada'><i class="fa-solid fa-gamepad-modern"></i><h5>Game Developer</h5>
+        <p>You can justify a higher price point for your products and services, if you go into details about your production values or ethically-sourced materials.</p>
+        <i class="fa-solid fa-browser" ></i><h5>Web Developer</h5>
+        <p>A company profile describes what makes your company unique. It automatically differentiates your brand because no other company has the exact same founding story  </p>
+        <i class="fa-solid fa-browser" ></i><h5>Web Developer</h5>
+        <p>A company profile describes what makes your company unique. It automatically differentiates your brand because no other company has the exact same founding story  </p></Col>
+      </Row>
+    </Container>
+    </div>
     </Parallax>
 );
 
