8da537ae0677ea19a2dc941f9f7027309cf99e15
SiroSanjaya22 <109472474+SiroSanjaya22@users.noreply.github.com>
2022-07-22 13:31:29 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,38 @@
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
-
+import foto from '../assets/logo192.png';
 function Header() {
   return (
     <>
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
     <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={foto}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            React Bootstrap
+          </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+         
+          
+         
         </Nav>
         <Nav>
-          <Nav.Link href="#deets">More deets</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            Dank memes
-          </Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#About Us">About Us</Nav.Link>
+          <NavDropdown title="Portfolio" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#web">Web Developer</NavDropdown.Item>
+            <NavDropdown.Item href="game">
+            Game Developer
+            </NavDropdown.Item>
+            <NavDropdown.Item href="3d">3D Artist</NavDropdown.Item>
+   
+          </NavDropdown>
         </Nav>
       </Navbar.Collapse>
     </Container>
