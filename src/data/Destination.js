85a9090dfb3ecbfb5db380a984df3ee640e44eef
SiroSanjaya22 <109472474+SiroSanjaya22@users.noreply.github.com>
2022-07-23 02:03:16 +0700

    Add files via upload

---
+++ src/data/Destination.js
@@ -0,0 +1,57 @@
+import scifi from '../assets/foto.jpg'
+const destination = [{
+        id: "https://www.lipsum.com/",
+        nama: "Restaurant",
+        paragraf: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        gambar: scifi,
+    },
+    {
+        id: 2,
+        nama: "Indonesiaku",
+        paragraf: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        gambar: scifi,
+    },
+    {
+        id: 3,
+        nama: "SKywal",
+        paragraf: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        gambar: scifi,
+
+    },
+    {
+        id: 4,
+        nama: "Semarang",
+        paragraf: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        gambar: scifi,
+
+    },
+    {
+        id: 5,
+        nama: "Lombok",
+        paragraf: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        gambar: scifi,
+
+    },
+    {
+        id: 6,
+        nama: "NTT",
+        paragraf: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        gambar: scifi,
+
+    },
+    {
+        id: 7,
+        nama: "Bandung",
+        paragraf: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        gambar: scifi,
+    },
+    {
+        id: 8,
+        nama: "Malang",
+        paragraf: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        gambar: scifi,
+
+    },
+];
+
+export default destination;
